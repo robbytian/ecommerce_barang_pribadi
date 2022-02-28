a24d0b1e9ae2f0701ffa78030392a34c7b5328ee
Robby Gustian <robbygustian11@gmail.com>
2022-02-28 07:01:58 +0700

    perbaikan eutik

--- resources/views/dashboard/fileKarya.blade.php
+++ resources/views/dashboard/fileKarya.blade.php
@@ -7,10 +7,10 @@
 <!-- Container Fluid-->
 <div class="container-fluid" id="container-wrapper">
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+        <h1 class="h3 mb-0 text-gray-800">List File Karya</h1>
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="./">Home</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+            <li class="breadcrumb-item"><a href="/dashboard">Dashboard</a></li>
+            <li class="breadcrumb-item active" aria-current="page">List File Karya </li>
         </ol>
     </div>
 
